c15dbfbc313fca6e7dd7a9a7d51cd6012dd1248d
didi0987 <didi0987@qq.com>
2015-10-28 15:37:37 -0500

    11

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-
+        Schedule s1=new Schedule();
         System.out.println("Hello World!");
         System.out.println("Hello World!");
-   System.out.print("");
+        System.out.print("");
 
     }
 
---
+++ src/Schedule.java
@@ -0,0 +1,17 @@
+/**
+ * Created by chenxiao on 10/28/2015.
+ */
+public class Schedule {
+
+    public double time;
+    public double price;
+
+    public Schedule(double time,double price){
+        this.price=price;
+        this.time=time;
+
+
+    }
+
+
+}
